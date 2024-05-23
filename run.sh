156bd974334cc211a96503f55bea97e49803f167
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-05-23 11:29:10 +0100

    changes to run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Trap signals and ensure cleanup
 trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
 
-# Store the current directory
-current_dir=$(pwd)
+# Store the relevent directories
+# Ollama directory
+OLLAMA="/homes/${USER}/Downloads/ollama"
+CROSSWORD="/homes/${USER}/Cryptic-Crossword-Reverse-Dictionary/"
+
 
 # Check if a config file is provided as an argument
 CONFIG_FILE="config.txt"  # Default config file
@@ -12,8 +15,6 @@ if [ $# -gt 0 ]; then
   CONFIG_FILE="$1"
 fi
 
-# Ollama directory
-OLLAMA="/homes/${USER}/Downloads/ollama"
 
 # Start the background service
 if [ -x $OLLAMA ]; then
@@ -24,7 +25,7 @@ else
 fi
 
 # Change back to the original directory
-cd "$current_dir" || exit
+cd $CROSSWORD || exit
 
 # Run the Python script with the specified config file
 python main.py --config "config_test/$CONFIG_FILE"
